f72bc5573dcbc66688ed6f37d8c0635cdda93950
yasin <163536619+YasinDav@users.noreply.github.com>
2025-06-30 17:52:12 +0330

    fix little problem with models and their control panel

--- core/consult/admin.py
+++ core/consult/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Consult, Question
 
-# Register your models here.
+admin.site.register(Consult)
+admin.site.register(Question)
--- core/consult/models.py
+++ core/consult/models.py
@@ -1,31 +1,43 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
-# get defult user model
+# get default user model
 User = get_user_model()
 
-# A General Class that creates for even users' request to consult
-# every information such as time, result of consult and questions or etc like them
-class Consult(models.Model):
 
+# A General Class that creates for even users' request to consult
+# every information such as time, result of consult and questions or etc. like them
+class Consult(models.Model):
     # time
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     # user
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    # status of consult model, that show the end of consulting
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id}_ {self.created_date.date()} - {self.user}"
+
+    # def get_absolute_url(self):
+
 
 class Question(models.Model):
-    # a text or command that ai module recive to generate question
-    # NOTE: for fisrt of all consult is same. but even module's anwer brings its self prompt to generate next question
+    # a text or command that AI module receive to generate question
+    # NOTE: for first of all consult is same. but even module's answer brings its self prompt to generate next question
     prompt = models.TextField()
 
-    # in use to that prompt that ai module recive, gives a question to ask from user
+    # in use to that prompt that AI module receive, gives a question to ask from user
     question = models.TextField()
 
-    # user's anwer to question
+    # user's answer to question
     answer = models.TextField()
 
     # a prompt else to generate another next question
     next_prompt = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}_ {self.question[:10]}... - {self.answer[:10]}..."
