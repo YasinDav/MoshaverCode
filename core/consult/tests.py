ce8d692c1b687b4953493c77803a8ffce6a40ae8
yasin <163536619+YasinDav@users.noreply.github.com>
2025-07-08 14:38:55 +0330

    testing simulator instead of Ai model

--- core/consult/tests.py
+++ core/consult/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+# from django.test import TestCase
+from random import choice
 
-# Create your tests here.
+question_type = ["input", "range"]
+
+questions = ["what is your favorite color?", "how do you feel your favorite color/s", "what do you hate the most?"]
+
+next_prompts = ["how many", "how many times", "how many times are"]
+
+
+def simulate_ai_request(t: str, **kwargs):
+    if t == "q":
+        return {"type": choice(question_type), "question": choice(questions)}
+    elif t == "a":
+        return choice(next_prompts)
