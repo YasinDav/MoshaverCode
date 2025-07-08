81f43d5801bbdd321f3b759c62b100972d335282
yasin <163536619+YasinDav@users.noreply.github.com>
2025-07-08 14:34:14 +0330

    complete django admin panel

--- core/consult/admin.py
+++ core/consult/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Consult, Question
 
-admin.site.register(Consult)
-admin.site.register(Question)
+
+# admin.site.register(Consult)
+# admin.site.register(Question)
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'consult', 'created_date', 'question', 'answer',)
+    list_filter = ('type', 'consult', 'created_date',)
+    search_fields = ('type', 'consult', 'answer', 'question',)
+    date_hierarchy = 'created_date'
+
+
+@admin.register(Consult)
+class ConsultAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_date', 'updated_date', 'status',)
+    list_filter = ('user', 'updated_date', 'created_date', 'status',)
